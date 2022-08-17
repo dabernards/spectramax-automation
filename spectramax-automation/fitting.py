bf7983cf8c0248d868ccbef0e3646a0be498c9b2
Dan Bernards <dabernards@gmail.com>
2022-08-16 19:28:14 -0700

    Reorganized heirarchy of fit functions so useless addition fit wasn't base

--- spectramax-automation/fitting.py
+++ spectramax-automation/fitting.py
@@ -5,19 +5,44 @@ import scipy.stats
 import scipy.optimize
 
 
-class GenericFit:
-    ''' For fitting, this provides a generic class to work with
-        By default this is a linear fit optimized by scipy.optimize.curve_fit
+class LinearFit:
+    ''' Base fitting class is a conventional linear fit. Uses
+        scipy.stats.linregress sicne it includes some linear-fit specific
+        results in fit_result
     '''
 
     def __init__(self):
         self.params=[]
         self.fit_result = []
 
-    def fit_func(self, x, a):    #pylint: disable=C0103,R0201
-        ''' Default function is addition '''
-        return a+x
+    def fit_func(self, x, intercept, slope):    #pylint: disable=C0103,W0221,R0201
+        ''' Default function is linear fit '''
+        return intercept + slope*x
 
+    def inv_func(self, y, intercept, slope):    #pylint: disable=C0103,R0201
+        ''' Fit function with swapped dependent/independent variables '''
+        return (y - intercept) / slope
+
+    def fit_method(self, xdata, ydata):
+        ''' For linear fit, use scipy.stats.linregress
+
+            Args:
+                -xdata: list of x data points
+                -ydata: list of y data points
+
+            Returns:
+                - params: set of fitting parameters for defined function
+                - fit_result: LinregressResult containing full detail of fit
+        '''
+        fit_result = scipy.stats.linregress(xdata, ydata)
+        self.params = fit_result.intercept, fit_result.slope
+        return (fit_result.intercept, fit_result.slope), fit_result
+
+
+class CurveFit(LinearFit):
+    ''' CurveFit provides the alternate fit method using scipy.optimize.curve_fit
+        Can be used by all other fit functions
+    '''
     def fit_method(self, xdata, ydata):
         ''' Default fit approach used scipy.optimize.curve_fit
             Result of fitting sets self.params as determined by fit and keeps
@@ -39,38 +64,26 @@ class GenericFit:
         return popt, [popt, pcov]
 
 
-class LinearFit(GenericFit):
-    ''' Even though linear function is default in genericFit incorporate here
-        to use scipy.stats.linregress instead since it includes a bit more
-        linear-fit specific details for fit_result
-    '''
-
-    def fit_func(self, x, intercept, slope):    #pylint: disable=C0103,W0221
-        ''' Default function is linear '''
-        return intercept + slope*x
-
-    def curve_fit(self, xdata, ydata):
-        ''' For linear fit, use scipy.stats.linregress
-
-            Args:
-                -xdata: list of x data points
-                -ydata: list of y data points
-
-            Returns:
-                - params: set of fitting parameters for defined function
-                - fit_result: LinregressResult containing full detail of fit
-        '''
-        fit_result = scipy.stats.linregress(xdata, ydata)
-        self.params = fit_result.intercept, fit_result.slope
-        return (fit_result.intercept, fit_result.slope), fit_result
-
-class PowerFit(GenericFit):
+class PowerFit(CurveFit):
     ''' Power function for it
     '''
     def fit_func(self, x, intercept, multip, power):    # pylint: disable=W0221
         ''' Most generic power-law function includes intercept '''
         return intercept + multip*x**power
 
+    def inv_func(self, y, intercept, multip, power):    # pylint: disable=W0221
+        return ( (y - intercept) / multip )**(1/power)
+
+
+class LogisticFit(CurveFit):
+    ''' Conventional 4-parameter logistic used for fitting ELISA data
+    '''
+    def fit_func(self, x, a, b, c, d):    # pylint: disable=W0221,R0913
+        ''' 4-parameter logistic function '''
+        return d + (a - d) / (1 + (x/c)**b )
+
+    def inv_func(self, y, a, b, c, d):    # pylint: disable=W0221,R0913
+        return c * ( (a - y) / (y - d) )**(1/b)
 
 
 if __name__ =='__main__':
